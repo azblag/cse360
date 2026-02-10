ef106e7ab6f97e208dd842b17438ec15645d70d7
blakeguinn101 <blakeguinn101@localhost>
2026-02-09 22:51:37 -0700

    to submit

--- cse360_A1_BlakeGuinn/src/application/Main.java
+++ cse360_A1_BlakeGuinn/src/application/Main.java
@@ -11,53 +11,61 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         // ----- Menu Bar -----
-        Menu fileMenu = new Menu("File");
+        Menu fileMenu = new Menu("Settings");
+        
+        MenuItem fullscreenItem = new MenuItem("Fullscreen");
+        fullscreenItem.setOnAction(event -> {
+            stage.setFullScreen(!stage.isFullScreen());
+        });
+        
         MenuItem exitItem = new MenuItem("Exit");
         exitItem.setOnAction(e -> stage.close());
+        
+        fileMenu.getItems().add(fullscreenItem);
         fileMenu.getItems().add(exitItem);
 
-        Menu helpMenu = new Menu("Help");
+        Menu infoMenu = new Menu("Info");
         MenuItem aboutItem = new MenuItem("About");
         aboutItem.setOnAction(e ->
                 new Alert(Alert.AlertType.INFORMATION,
-                        "Simple JavaFX GUI\nCreated by Blake Guinn").showAndWait()
+                        "Assignment 1 / February 9th, 2026 / Blake Guinn").showAndWait()
         );
-        helpMenu.getItems().add(aboutItem);
+        infoMenu.getItems().add(aboutItem);
 
-        MenuBar menuBar = new MenuBar(fileMenu, helpMenu);
+        MenuBar menuBar = new MenuBar(fileMenu, infoMenu);
 
-        // ----- Form Fields -----
-        Label nameLabel = new Label("Name:");
-        TextField nameField = new TextField("Blake Guinn");
+        // ----- Form Fields ----- //
+        Label nameLabel = new Label("Username:");
+        TextField nameField = new TextField("");
 
-        Label programLabel = new Label("Program:");
-        TextField programField = new TextField("JavaFX GUI Assignment");
+        Label colorLabel = new Label("Favorite Color:");
+        TextField colorField = new TextField("");
 
         HBox nameBox = new HBox(10, nameLabel, nameField);
-        HBox programBox = new HBox(10, programLabel, programField);
+        HBox programBox = new HBox(10, colorLabel, colorField);
 
-        // ----- Buttons -----
+        // ----- Buttons ----- //
         Button submitButton = new Button("Submit");
         Button clearButton = new Button("Clear");
 
         submitButton.setOnAction(e ->
                 new Alert(Alert.AlertType.INFORMATION,
-                        "Submitted by: " + nameField.getText()).showAndWait()
+                        nameField.getText() + " has a favorite color and it is color it is " + colorField.getText() + "!").showAndWait()
         );
 
         clearButton.setOnAction(e -> {
             nameField.clear();
-            programField.clear();
+            colorField.clear();
         });
 
         HBox buttonBox = new HBox(10, submitButton, clearButton);
 
-        // ----- Layout -----
+        // ----- Layout ----- //
         VBox layout = new VBox(15, menuBar, nameBox, programBox, buttonBox);
         layout.setStyle("-fx-padding: 15;");
 
-        // ----- Scene -----
-        Scene scene = new Scene(layout, 400, 250);
+        // ----- Scene ----- //
+        Scene scene = new Scene(layout, 350, 200);
         stage.setTitle("Simple JavaFX GUI - Blake Guinn");
         stage.setScene(scene);
         stage.show();
